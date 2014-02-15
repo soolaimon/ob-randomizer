e33a38c5426274df9b2f76e90b46a0c7249d79ad
Dave Nelson <davidicus21@gmail.com>
2014-02-14 18:50:48 -0700

    Add heroku provided port

--- app.js
+++ app.js
@@ -6,5 +6,7 @@ app.get('/', function(req, res) {
 	res.sendfile("index.html");
 });
 
-app.listen(3000);
-sys.puts("Server running on 3000");
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
