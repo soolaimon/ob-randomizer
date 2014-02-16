0ad001a32b9755e4431c3f4665b51eff4d09d6ae
Dave Nelson <davidicus21@gmail.com>
2014-02-15 21:52:14 -0700

    Fixed css link

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ var sys = require("sys");
 var express = require("express");
 var app = express();
 
+app.configure(function() {
+        app.use(express.static(__dirname + '/public'));
+});
+
 app.get('/', function(req, res) {
 	res.sendfile("index.html");
 });
+
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
